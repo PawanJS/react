b4f20e1438defea695edeb5e1e903fd1cf73ca6e
PawanJS <contactpawanjs@gmail.com>
2021-12-07 06:58:02 +0530

    Added a handleChange function in app.js

--- Cats_Paradise/src/App.js
+++ Cats_Paradise/src/App.js
@@ -21,6 +21,10 @@ class App extends React.Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
@@ -29,9 +33,10 @@ class App extends React.Component {
 
     return (
       <div className="App">
+        <h1>Cats Paradise</h1>
         <SearchBox
           placeholder="Search Cute Cats"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
