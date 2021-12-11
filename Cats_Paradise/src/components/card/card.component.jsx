983dc518685f8f00e974828adfb1e56c5c06161a
PawanJS <contactpawanjs@gmail.com>
2021-12-11 22:50:44 +0530

    Added new project

--- Cats_Paradise/src/components/card/card.component.jsx
+++ Cats_Paradise/src/components/card/card.component.jsx
@@ -10,7 +10,7 @@ export const Card = (props) => {
         src={`https://robohash.org/${props.monster.id}1?set=set4&size=180x180`}
       />
       <h2 key={props.monster.id}>{props.monster.name}</h2>
-      <p>{props.monster.email}</p>
+      <>{props.monster.email}</>
     </div>
   );
 };
---
+++ photo_world/src/Private/dont-open.js
@@ -0,0 +1,1 @@
+export const client_id = 'q8z_R4ilRnTWOaSiJhF2yuL08e4ZCrcKstzGJ0CmP5g';
