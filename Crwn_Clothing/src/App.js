8f38d396c237a63cb00898059472588228a00609
PawanJS <contactpawanjs@gmail.com>
2021-12-13 21:11:40 +0530

    Added React Router

--- Crwn_Clothing/src/App.js
+++ Crwn_Clothing/src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import HomePage from './Pages/HomePage/homepage.component';
 
 import './App.css';
 
+const Hats = () => {
+  return <div>I am hats</div>;
+};
+
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/hats" element={<Hats />} />
+      </Routes>
     </div>
   );
 }
