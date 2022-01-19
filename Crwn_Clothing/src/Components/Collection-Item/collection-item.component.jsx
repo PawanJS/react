6805208f62fb18722086ac2b8bb66ffcae33836b
PawanJS <contactpawanjs@gmail.com>
2022-01-19 22:16:51 +0530

    Removed test code

--- Crwn_Clothing/src/Components/Collection-Item/collection-item.component.jsx
+++ Crwn_Clothing/src/Components/Collection-Item/collection-item.component.jsx
@@ -9,6 +9,7 @@ const CollectionItem = ({ imageUrl, name, price }) => {
       <div className="collection-footer">
         <span className="name">{name}</span>
         <span className="price">{price}</span>
+        <span>hhh</span>
       </div>
     </div>
   );
