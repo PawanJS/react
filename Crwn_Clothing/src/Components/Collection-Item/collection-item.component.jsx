2d30eddac4ecc2cc25ce5e4c1010dec86aed3dc4
PawanJS <contactpawanjs@gmail.com>
2022-01-19 21:05:09 +0530

    Test

--- Crwn_Clothing/src/Components/Collection-Item/collection-item.component.jsx
+++ Crwn_Clothing/src/Components/Collection-Item/collection-item.component.jsx
@@ -9,6 +9,7 @@ const CollectionItem = ({ imageUrl, name, price }) => {
       <div className="collection-footer">
         <span className="name">{name}</span>
         <span className="price">{price}</span>
+        <span className="price">{price}</span>
       </div>
     </div>
   );
